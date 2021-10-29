d88a8a942c3bfe010601321255d826a460c16b67
Jolabuse <jonathan.witt@efrei.net>
2021-10-29 11:50:21 +0200

    really adding vehicle.cs

---
+++ DEIsaWITT_Jonathan_Ex2/Vehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DEIsaWITT_Jonathan_Ex2
+{
+    public interface IVehicule
+    {
+        bool IsMoving { get; set; }
+        int WheelsCount { get; set; }
+    }
+
+    public abstract class AVehicle : IVehicule
+    {
+        public bool IsMoving { get; set; }
+        public int WheelsCount { get; set; }
+        protected bool CanFly { get; set; }
+    
+        public abstract bool Drive();
+        public abstract bool Stop();
+    }
+    
+}
